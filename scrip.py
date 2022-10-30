a3bb4310771c3ab3db8d0f8e1e07b80668009063
InItIer008 <117005168+InItIer008@users.noreply.github.com>
2022-10-30 09:50:35 +0100

    Add files via upload

---
+++ scrip.py
@@ -0,0 +1,122 @@
+#CODING:utf-8
+# -*- coding:utf-8 -*
+# un commentaire
+""" 
+commmenter sur plusieurs lignes
+les espaces blancs sont pris en compte
+\n pour aller a la ligne
+\t pour la tabulation
+
+"""
+
+
+# from pickle import TRUE
+
+
+print("\tbonjour tout le monde")
+
+print("je suis une\n autre phrase")
+
+#variales 
+#une variable est un espace de stockage , elle permet de stocker temporerement les donnes dans la memoire de votre ordinateur
+"""
+nommer une variable : doit commencer par une lettre ou un underscore
+                      ne doit pas contenir de caracteres speciaux
+                      ne pas contenir d'espace
+                      Utiliser des underscore (_)
+"""
+
+"""
+_agePersonne
+age_Personne
+"""
+
+"""
+les types de donees standards : entier numerique (int)
+                                nombre flottant  (float)
+                                chaine de caractere (str)
+                                booléen (bool)
+
+"""
+
+print("Mr COULUMBA " * 5)
+
+
+
+
+agePersonne=18+5 #variable de type entier int
+
+
+print(agePersonne)
+
+taillePersone=1.8 #variable de type flottant float
+
+print(type(agePersonne))
+
+print(type(taillePersone))
+
+
+lieuPersonne="LOME" #une chaine de caractere est une variable de type string str
+
+print(type(lieuPersonne))
+
+
+genreMasculin= True #variabla de type booleen  bool
+
+genreFeminin= False
+
+
+
+
+print(type(genreMasculin))
+
+print(type(genreFeminin))
+
+
+ageAutrePersonne=14
+
+print("la personne a ",ageAutrePersonne) #csa s appelle une concatenation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
